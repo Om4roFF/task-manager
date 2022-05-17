698aba0bbe65ebc55737fb755b969a76ccd3225c
omarov.b <omarov.b@aster-auto.kz>
2022-05-17 17:35:33 +0600

    update

--- db/sessions.py
+++ db/sessions.py
@@ -7,7 +7,7 @@ sessions = sqlalchemy.Table(
     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True, autoincrement=True, unique=True),
     sqlalchemy.Column("user_id", sqlalchemy.Integer, sqlalchemy.ForeignKey('users.id')),
     sqlalchemy.Column("latitude", sqlalchemy.Float),
-    sqlalchemy.Column("longitude ", sqlalchemy.Float),
+    sqlalchemy.Column("longitude", sqlalchemy.Float),
     sqlalchemy.Column("started_at", sqlalchemy.DateTime,),
     sqlalchemy.Column("finished_at", sqlalchemy.DateTime,),
 )
