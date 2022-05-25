402dc06121008fdf7950d539d9cd61d3fa9af6f0
omarov.b <omarov.b@aster-auto.kz>
2022-05-25 14:36:03 +0600

    update

--- db/__init__.py
+++ db/__init__.py
@@ -2,7 +2,7 @@ from .base import metadata, engine
 from .users import users
 from .auth import auth
 from .cities import cities
-# from .comments import comments
+from .comments import comments
 from .boards import boards
 from .groups import groups
 from .task import tasks
