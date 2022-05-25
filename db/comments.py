3adc4e6d4376808a8e7098fa0009018b76030171
omarov.b <omarov.b@aster-auto.kz>
2022-05-25 14:38:48 +0600

    update

--- db/comments.py
+++ db/comments.py
@@ -8,7 +8,7 @@ comments = sqlalchemy.Table(
     metadata,
     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True, autoincrement=True, unique=True),
     sqlalchemy.Column("content", sqlalchemy.String),
-    sqlalchemy.Column("user_id", sqlalchemy.String, sqlalchemy.ForeignKey("users.id")),
+    sqlalchemy.Column("user_id", sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id")),
     sqlalchemy.Column("task_id", sqlalchemy.Integer, sqlalchemy.ForeignKey("tasks.id")),
     sqlalchemy.Column("created_at", sqlalchemy.DateTime, default=datetime.datetime.utcnow),
     sqlalchemy.Column("updated_at", sqlalchemy.DateTime, default=datetime.datetime.utcnow)
